1c25534d546b83541f4e31e818b5e40d78bb96e7
Yegor Zhmak <yegor.zhmak@eltex.loc>
2022-04-14 16:16:17 +0700

    Добавлена анимация

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ console.warn("Сообщение-предупреждение");
 
 function showModal(messageText, buttonText) {
     let modal = document.getElementsByClassName('modal')[0];
-    modal.style.display = 'block';
+    modal.style.visibility = 'visible';
+    modal.style.opacity = '1';
     
     let message = modal.getElementsByClassName('message')[0];
     message.innerHTML = messageText;
@@ -13,15 +14,22 @@ function showModal(messageText, buttonText) {
 
     document.body.style.overflow = 'hidden';
     let overlay = document.getElementsByClassName('overlay')[0];
-    overlay.style.display = 'block';
+    overlay.style.visibility = 'visible';
+    overlay.style.opacity = '1';
 }
 function hideModal() {
     let modal = document.getElementsByClassName('modal')[0];
-    modal.style.display = 'none';
+    setTimeout(function() {
+        modal.style.visibility = 'hidden';
+    }, 350); // 200ms + 150ms
+    modal.style.opacity = '0';
 
     document.body.style.overflow = 'auto';
     let overlay = document.getElementsByClassName('overlay')[0];
-    overlay.style.display = 'none';
+    setTimeout(function() {
+        overlay.style.visibility = 'hidden';
+    }, 200);
+    overlay.style.opacity = '0';
 }
 function notReadyAlert() {
     showModal('Sorry, not ready yet!<br>Извините, ещё не готово!', 'Эх, жаль');
